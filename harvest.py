7dd6007a14cb5e150b0d4f18f98803cc9c69e2ac
Norris Mei <norrismei3@gmail.com>
2020-07-16 23:09:39 +0000

    Completed Melon class, and funcs to instantiate melons and get sellability report

--- harvest.py
+++ harvest.py
@@ -113,31 +113,31 @@ def make_melons(melon_types):
 
     melons = []
 
-    melon_1 = Melon(melon_type_lookup['yw'], 8, 7, 2, 'Sheila')
+    melon_1 = Melon(melon_types['yw'], 8, 7, 2, 'Sheila')
     melons.append(melon_1)
 
-    melon_2 = Melon(melon_type_lookup['yw'], 3, 4, 2, 'Sheila')
+    melon_2 = Melon(melon_types['yw'], 3, 4, 2, 'Sheila')
     melons.append(melon_2)
 
-    melon_3 = Melon(melon_type_lookup['yw'], 9, 8, 3, 'Sheila')
+    melon_3 = Melon(melon_types['yw'], 9, 8, 3, 'Sheila')
     melons.append(melon_3)
 
-    melon_4 = Melon(melon_type_lookup['cas'], 10, 6, 35, 'Sheila')
+    melon_4 = Melon(melon_types['cas'], 10, 6, 35, 'Sheila')
     melons.append(melon_4)
 
-    melon_5 = Melon(melon_type_lookup['cren'], 8, 9, 35, 'Michael')
+    melon_5 = Melon(melon_types['cren'], 8, 9, 35, 'Michael')
     melons.append(melon_5)
 
-    melon_6 = Melon(melon_type_lookup['cren'], 8, 2, 35, 'Michael')
+    melon_6 = Melon(melon_types['cren'], 8, 2, 35, 'Michael')
     melons.append(melon_6)
 
-    melon_7 = Melon(melon_type_lookup['cren'], 2, 3, 4, 'Michael')
+    melon_7 = Melon(melon_types['cren'], 2, 3, 4, 'Michael')
     melons.append(melon_7)
 
-    melon_8 = Melon(melon_type_lookup['musk'], 6, 7, 4, 'Michael')
+    melon_8 = Melon(melon_types['musk'], 6, 7, 4, 'Michael')
     melons.append(melon_8)
 
-    melon_9 = Melon(melon_type_lookup['yw'], 7, 10, 3, 'Sheila')
+    melon_9 = Melon(melon_types['yw'], 7, 10, 3, 'Sheila')
     melons.append(melon_9)
 
     return melons
@@ -146,7 +146,18 @@ def make_melons(melon_types):
 def get_sellability_report(melons):
     """Given a list of melon object, prints whether each one is sellable."""
 
-    # Fill in the rest 
+    for melon in melons:
+        if melon.is_sellable():
+            is_sellable_msg = '(CAN BE SOLD)'
+        else:
+            is_sellable_msg = '(NOT SELLABLE)'
+        print("Harvested by " + melon.harvested_by + " from Field " + \
+            str(melon.from_field) + " " + is_sellable_msg)
 
 
+# Functions to test script
+melon_types = make_melon_types()
+melon_dict = make_melon_type_lookup(melon_types)
+melons = make_melons(melon_dict)
+get_sellability_report(melons)
 
